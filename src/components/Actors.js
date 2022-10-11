476ef8d5cde33a92cc6db53c03a9871ee569afc4
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-11 11:59:12 +0300

    complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+
+      {actors.map((actor) => {
+        return (
+          <div key={actor.name}>
+            <h2>{actor.name}</h2>
+            <li>{actor.movies}</li>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Actors;
